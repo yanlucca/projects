f24fa220da5f5b618e3aa3fcaa005f816064d8c2
Yan Lucca <118584394+yanlucca@users.noreply.github.com>
2025-12-16 18:46:00 -0300

    Add files via upload

---
+++ Simple React Footer/index.jsx
@@ -0,0 +1,31 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <ul>
+        <li class="footer-item">
+          <a href="#">Thing 1</a>
+        </li>
+        <li class="footer-item">
+          <a href="#">Thing 2</a>
+        </li>
+      </ul>
+      <ul>
+        <li class="footer-item">
+          <a href="#">Thing 3</a>
+        </li>
+        <li class="footer-item">
+          <a href="#">Thing 4</a>
+        </li>
+      </ul>
+      <ul>
+        <li class="footer-item">
+          <a href="#">Thing 5</a>
+        </li>
+        <li class="footer-item">
+          <a href="#">Thing 6</a>
+        </li>
+      </ul>
+      <p>© 2025 Yan Lucca Footer. No rights reserved.</p>
+    </footer>
+  );
+};
